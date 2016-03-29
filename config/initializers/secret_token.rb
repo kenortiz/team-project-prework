139e87cf244852260d7f4ae2f540b087289aa107
Kenneth Ortiz <kenortiz@users.noreply.github.com>
2016-03-29 04:36:29 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamProjectPrework::Application.config.secret_key_base = '00dfe289310062ef4b552b78fafaf62cb97f3bac188d450225cfa2fcb495b402f73d623b07441c0e70abd2c8b559117a366266f0bb78cd283d22fcd4ea3dd7aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeamProjectPrework::Application.config.session_store :cookie_store, key: '_team-project-prework_session'
